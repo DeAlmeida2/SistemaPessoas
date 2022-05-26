4287c4a6651abd1a1fd626db8ec1e3d2fca8c662
Andre Felipe <andre_f_al@hotmail.com>
2022-05-25 22:01:03 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio25.05.22/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio25._05._22
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public string cidade;
+        public int idade;
+        public string peso;
+        public string altura;
+
+        public Pessoa(string nome, string cidade, int idade, string peso, string altura)
+        {
+            this.nome = nome;
+            this.cidade = cidade;
+            this.idade = idade;
+            this.peso = peso;
+            this.altura = altura;
+        }
+    }
+}
---
+++ exercicio25.05.22/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using exercicio25._05._22;
+
+Console.WriteLine("Estamos fazendo uma entrevista com você, por favor responda esse questionário!");
+String nome, cidade, altura, idade;
+int peso;
+Console.WriteLine("Digite o nome da pessoa: ");
+nome = Console.ReadLine();
+Console.WriteLine("Digite a cidade natal que ela nasceu: ");
+cidade = Console.ReadLine();
+Console.WriteLine("Digite o peso desta pessoa: ");
+peso = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a altura desta pessoa: ");
+altura = (Console.ReadLine());
+Console.WriteLine("Digite a idade desta pessoa: ");
+idade = (Console.ReadLine());
+
+Pessoa P1 = new Pessoa (nome, cidade, peso, altura, idade);
+
+Console.WriteLine("O nome da pessoa é: " + nome + " a sua cidade natal é " + cidade + " ela pesa " + peso + "Kg, e mede " + altura + " ahhhh! ela tem " + idade + " anos.");
